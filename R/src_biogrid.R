5aa302c36c1007e57ab8adb08fdaff01980761a3
Eric Bryant <EricEdwardBryant@gmail.com>
2017-08-28 10:25:09 -0400

    Fixes for dplyr 0.7

--- R/src_biogrid.R
+++ R/src_biogrid.R
@@ -74,32 +74,59 @@ src_biogridr <- function(path = getOption('biogridr.db')) {
             call. = FALSE)
   }
   list(con = con, path = path, release = release, info = dbGetInfo(con)) %>%
-    assign_class('src_biogridr', 'src_sqlite', 'src_sql', 'src')
+    add_class('src_biogridr')
 }
 
-#=========== src_desc ============
-#' @importFrom dplyr src_desc
+#---- src_biogridr methods --------------------------------------------------
 #' @export
-src_desc.src_biogridr <- function(x) {
-  paste0('sqlite ', x$info$serverVersion, ' [', x$release, ']')
+print.src_biogridr <- function(x) {
+  cat(paste0(
+    'sqlite ', RSQLite::rsqliteVersion()[[2]], 
+    ' [biogridr - ', x$release, ']\n',
+    dplyr:::wrap('tbls: ', paste0(RSQLite::dbListTables(x$con), collapse = ', '))
+  ))
 }
 
-#========== tbl ==================
-#' @importFrom dplyr tbl tbl_sql
+#' @importFrom dbplyr tbl_sql
 #' @export
 tbl.src_biogridr <- function(src, from, ...) {
-  src %>% 
-    drop_class %>%
-    tbl(from) %>%
-    assign_class('tbl_biogridr', class(.))
+  dbplyr::tbl_sql('sqlite', src = src, from = from, ...) %>% add_class('tbl_biogridr')
 }
 
-#========= collect ==============
-#' @importFrom dplyr collect
 #' @export
-collect.tbl_biogridr <- function(tbl) {
-  tbl %>%
-    drop_class %>%
-    collect %>%
-    assign_class('tbl_biogridr', class(.))
+collect.tbl_rothfreezer <- function(x, ...) {
+  
+  result <- drop_class(x) %>% collect(...)
+  coltypes <- x$src$cnf$column_types
+  present <- intersect(names(coltypes), names(result))
+  
+  if (length(present)) {
+    coerce <- coltypes[present]
+    for (i in 1:length(coerce)) {
+      type   <- coerce[[i]]
+      column <- names(coerce[i])
+      result[[column]] <- 
+        switch(
+          type,
+          logical   = as.logical(result[[column]]),
+          integer   = as.integer(result[[column]]),
+          character = as.character(result[[column]]),
+          numeric   = as.numeric(result[[column]]),
+          double    = as.numeric(result[[column]]),
+          factor    = as.factor(result[[column]])
+        )
+    }
+  }
+  # Coerce to desired column type
+  return(result)
 }
+
+#---- OO Utilities ------------------------------------------------------------
+# Add an S3 subclass in pipeline
+# @param x an S3 object
+# @param ... Character; S3 subclasses to add to object
+add_class <- function(x, ...) { class(x) <- c(..., class(x)); return(x) }
+
+# Drop an S3 subclass in pipeline
+# @param x an S3 object
+drop_class <- function(x) { class(x) <- class(x)[-1]; return(x)}
--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,3 @@
-# Class assignment for use in dplyr piplelines
-assign_class <- function(x, ...) { class(x) <- c(...); return(x) }
-
-# Remove first class e.g. c('tbl_df', 'data.frame') to 'data.frame' 
-drop_class <- function(x) assign_class(x, class(x)[-1])
-
 # Attribute assignment for use in dplyr pipelines
 assign_attr <- function(x, which, ...) { attr(x, which) <- c(...); return(x) }
 
@@ -26,12 +20,12 @@ set_bounds <- function(x, lower, upper) {
 as.tbl.hclust <- function(x) {
   as_data_frame(x[c('labels', 'order')]) %>%
     assign_attr('hclust', x) %>%
-    assign_class('tbl_hclust', class(.))
+    add_class('tbl_hclust')
 }
 
 #' @importFrom stats as.hclust
 #' @export
-as.hclust.tbl_hclust <- function(x) attr(x, 'hclust') %>% assign_class('hclust')
+as.hclust.tbl_hclust <- function(x) attr(x, 'hclust') %>% add_class('hclust')
 
 # From Hadley Wickham. I use it to give NULL arguments default values
 `%||%` <- function(a, b) if (is.null(a)) b else a
